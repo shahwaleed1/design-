8abe05444989558f05f3be04ada9003f7999de7e
shahwaleed1 <its.waleed33@gmail.com>
2025-07-10 22:26:08 +0500

    footer responsive

--- bircube/components/Footer.jsx
+++ bircube/components/Footer.jsx
@@ -7,19 +7,20 @@ import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className='bg-[#242424]'>
-        <div className='container py-6'>
-            <hr />
-            <div className='flex justify-between mt-5 py-2'>
-                <div className='text-xl font-semibold'>
-                    adamkeyes
+            <div className='container py-6'>
+                <hr />
+                <div className='flex flex-col sm:flex-row justify-between items-center mt-5 py-2 text-center sm:text-left'>
+                    <div className='text-xl font-semibold mb-2 sm:mb-0'>
+                        adamkeyes
+                    </div>
+                    <div className='flex gap-4 text-lg'>
+                        <FaGithub />
+                        <FaLinkedin />
+                        <FaTwitter />
+                    </div>
                 </div>
-                <div className='flex gap-4 text-lg'>
-                    <FaGithub />
-                    <FaLinkedin />
-                    <FaTwitter />
-                </div>
+
             </div>
-        </div>
         </div>
     )
 }
